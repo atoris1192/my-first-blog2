e9397996fa36a7d0c1750b0ef0bfbb63da2b3c77
atoris1192 <atoris1192@gmail.com>
2019-10-29 17:27:18 +0900

    html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post # Post 関数呼び出し
+## published_date is collect
 def post_list(request):
-  return render(request, 'blog/post_list.html', {})
+  posts = Post.objects.filter(published_data__lte=timezone.now()).order_by('published_data')
+  return render(request, 'blog/post_list.html', {'posts': posts})
